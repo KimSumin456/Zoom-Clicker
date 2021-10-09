e34adc5f76edbc0bbcbe2a655ecde49da45092e4
KimSumin <ajou456@ajou.ac.kr>
2021-10-09 23:19:07 +0900

    Update Clicker() - Fixing the program window in the front
    명령어 실행 후 다시 ZoomClicker 프로그램 창을 맨 앞으로 띄우는 기능 추가

--- main.py
+++ main.py
@@ -52,47 +52,63 @@ def CrawlTimeline():
     f.close()
 
 def Clicker():
-    tooltips = ""
     f = open('tooltips.txt', 'r', encoding='UTF-8')
     tooltips = f.readlines()
     f.close()
 
     x_start = 70
     x_end = 1350
-    y = 930
-    menu = 'k'
+    y = 935
+    menu = '0'
+    btn_play = driver.find_element_by_xpath('//*[@id="vjs_video_3"]/div[4]/button')
     while menu != 'e':
         menu = input('명령어를 입력하세요: ')
         pyautogui.moveTo(x_start, y)
+        time.sleep(1)
         timeRangeCurrent = driver.find_element_by_class_name('vjs-time-range-current').text
         cur_hour = int(timeRangeCurrent[1])
         cur_minute = int(timeRangeCurrent[3]) * 10 + int(timeRangeCurrent[4])
         cur_second = int(timeRangeCurrent[6]) * 10 + int(timeRangeCurrent[7])
-        i = len(tooltips)
-        if menu == 'j':
+        tooltips_len = len(tooltips)
+        if len(menu) == 0:
+            continue
+        if menu[0] == '5':
+            btn_play.click()
+            continue
+        elif menu[0] == '4':
+            i = tooltips_len
             while i >= 0:
                 i -= 1
                 print(tooltips[i])
                 tip_hour = int(tooltips[i][0])
                 tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
                 tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
-                if ((
-                        cur_hour == tip_hour and cur_minute == tip_minute and cur_second - 10 <= tip_second <= cur_second - 5)
-                        or (cur_hour == tip_hour and cur_minute - 1 == tip_minute and 50 <= tip_second <= 59)
-                        or (cur_hour - 1 == tip_hour and cur_minute == 00 and 50 <= tip_second <= 59)):
-                    pyautogui.moveTo(x_start + i, y)
+                if ((cur_hour == tip_hour and cur_minute == tip_minute and cur_second - 10 <= tip_second <= cur_second - 5)
+                    or (cur_hour == tip_hour and cur_minute - 1 == tip_minute and 50 <= tip_second <= 59)
+                    or (cur_hour - 1 == tip_hour and cur_minute == 00 and 50 <= tip_second <= 59)):
+                    pyautogui.moveTo(x_start + i - (len(menu) - 1), y) # 문자 여러 개 입력한 만큼 더 ㄱㄱ
                     break
-        elif menu == 'l':
-            while i >= 0:
-                i -= 1
+        elif menu[0] == '6':
+            i = 0
+            while i < tooltips_len:
+                i += 1
+                tip_hour = int(tooltips[i][0])
                 tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
                 tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
-                if ((cur_minute == tip_minute and cur_second + 5 <= tip_second <= cur_second + 10)
-                        or (cur_hour == tip_hour and cur_minute + 1 == tip_minute and 0 <= tip_second <= 10)
-                        or (cur_hour + 1 == tip_hour and cur_minute == 59 and 0 <= tip_second <= 10)):
-                    pyautogui.moveTo(x_start + i, y)
+                if ((cur_hour == tip_hour and cur_minute == tip_minute and cur_second + 5 <= tip_second <= cur_second + 10)
+                    or (cur_hour == tip_hour and cur_minute + 1 == tip_minute and 0 <= tip_second <= 10)
+                    or (cur_hour + 1 == tip_hour and cur_minute == 59 and 0 <= tip_second <= 10)):
+                    pyautogui.moveTo(x_start + i + (len(menu) - 1) + 2, y) # 2는 수동 보정. 왜냐하면 이거 하는 사이에 영상이 계속 재생되니까. 그리고 보통 영상 넘길 땐 팍팍 넘기니까
                     break
+        else:
+            continue
         pyautogui.click()
+        pyautogui.hotkey('alt', 'tab') # 다시 ZoomClicker 프로그램 창 띄우기
+        time.sleep(1)
+        pyautogui.moveTo(x_end, y) # 그냥 내가 타임 테이블 계속 보고 싶어서
+
+    driver.close()
+    return 0
 
 
 options = Options()
@@ -102,8 +118,8 @@ options.add_argument("--start-maximized")  # 최대화
 driver = Chrome("chromedriver_win32\chromedriver", options=options)
 
 OpenZoom(
-    'https://···.zoom.us/rec/play/···',
-    '···')
+    'https://ajou-ac-kr.zoom.us/rec/play/0NfxxPyRNHYklG9rQdKJIgGuHulsSUeMRChCOyJ7caOXxWiE-rUJOuC0NtgyoAWCDpZTSvmY4njhfBjN.UcPBJoQ-MYxgLRAE?continueMode=true&_x_zm_rtaid=XzEet155S32YMo6roMZXAw.1633655817998.cb8abb1a013236c89248563d3c7d96ca&_x_zm_rhtaid=114',
+    '3m!CnemD')
 
 # CrawlTimeline()
 
