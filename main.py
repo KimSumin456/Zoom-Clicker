1a679605d7e4c8d820257fc580fc02f15415506c
KimSumin <ajou456@ajou.ac.kr>
2021-10-13 01:18:07 +0900

    Modify Clicker() - Handle the version without a chating(transcript) / Modify main() - Add the user input processing
    사용자가 입력한 강의 제목의 텍스트 파일에 타임라인(tooltips)을 저장

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import selenium
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
@@ -5,10 +7,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pyautogui
 
+# 채팅 있을 때
+# x_start: 62~74 -> 70
+# x_end: 1348 ~ 1361 -> 1350
 
-# x_start: 62~74
-# x_end: 1348 ~ 1361
-def OpenZoom(target_url, pw):
+# 채팅 없을 때
+# x_start: 178~191 -> 190
+# x_end: 1719~1731 -> 1720
+def OpenZoom(target_url, pw, lecture_name):
     driver.get(target_url)
     time.sleep(1)
 
@@ -23,10 +29,23 @@ def OpenZoom(target_url, pw):
     btn_mute = driver.find_element_by_xpath('//*[@id="vjs_video_3"]/div[4]/div[3]/button')
     btn_mute.click()
 
-def CrawlTimeline():
-    x_start = 70
-    x_end = 1350
-    y = 930
+def CrawlTimeline(lecture_name):
+    try:
+        f = open(lecture_name + '.txt', 'r', encoding='UTF-8')
+    except FileNotFoundError:
+        print("타임라인 스캔을 시작합니다. 반드시 마우스를 움직이지 말고 기다려주세요.")
+        time.sleep("3")
+    else:
+        return 0
+
+    try:
+        y = 930
+        x_start = 70
+        x_end = 1350
+        driver.find_element_by_class_name("transcript-wrapper")
+    except selenium.common.exceptions.NoSuchElementException:
+        x_start = 190
+        x_end = 1720
     tooltips = ""
     for i in range(x_start, x_end):
         i += 2
@@ -45,20 +64,27 @@ def CrawlTimeline():
         '''
 
         tooltip = driver.find_element_by_xpath('//*[@id="vjs_video_3"]/div[4]/div[1]/div[1]/div[2]/div').text
-        tooltips += tooltip
+        tooltips += tooltip + "\n"
 
-    f = open('tooltips.txt', 'w', encoding='UTF-8')
+    f = open(lecture_name+'.txt', 'w', encoding='UTF-8')
     f.write(tooltips)
     f.close()
+    print("타임라인 스캔을 종료합니다.")
 
-def Clicker():
-    f = open('tooltips.txt', 'r', encoding='UTF-8')
+def Clicker(lecture_name):
+    f = open(lecture_name+'.txt', 'r', encoding='UTF-8')
     tooltips = f.readlines()
     f.close()
 
-    x_start = 70
-    x_end = 1350
-    y = 935
+    try:
+        y = 930
+        x_start = 70
+        x_end = 1350
+        driver.find_element_by_class_name("transcript-wrapper")
+    except selenium.common.exceptions.NoSuchElementException:
+        x_start = 190
+        x_end = 1720
+
     menu = '0'
     btn_play = driver.find_element_by_xpath('//*[@id="vjs_video_3"]/div[4]/button')
     while menu != 'e':
@@ -80,9 +106,15 @@ def Clicker():
             while i >= 0:
                 i -= 1
                 print(tooltips[i])
-                tip_hour = int(tooltips[i][0])
-                tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
-                tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
+                if len(tooltips[0]) == 6:
+                    tip_hour = 0
+                    tip_minute = int(tooltips[i][0]) * 10 + int(tooltips[i][1])
+                    tip_second = int(tooltips[i][3]) * 10 + int(tooltips[i][4])
+                else:
+                    tip_hour = int(tooltips[i][0])
+                    tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
+                    tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
+
                 if ((cur_hour == tip_hour and cur_minute == tip_minute and cur_second - 10 <= tip_second <= cur_second - 5)
                     or (cur_hour == tip_hour and cur_minute - 1 == tip_minute and 50 <= tip_second <= 59)
                     or (cur_hour - 1 == tip_hour and cur_minute == 00 and 50 <= tip_second <= 59)):
@@ -92,9 +124,16 @@ def Clicker():
             i = 0
             while i < tooltips_len:
                 i += 1
-                tip_hour = int(tooltips[i][0])
-                tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
-                tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
+                print(tooltips[i])
+                if len(tooltips[0]) == 6:
+                    tip_hour = 0
+                    tip_minute = int(tooltips[i][0]) * 10 + int(tooltips[i][1])
+                    tip_second = int(tooltips[i][3]) * 10 + int(tooltips[i][4])
+                else:
+                    tip_hour = int(tooltips[i][0])
+                    tip_minute = int(tooltips[i][2]) * 10 + int(tooltips[i][3])
+                    tip_second = int(tooltips[i][5]) * 10 + int(tooltips[i][6])
+
                 if ((cur_hour == tip_hour and cur_minute == tip_minute and cur_second + 5 <= tip_second <= cur_second + 10)
                     or (cur_hour == tip_hour and cur_minute + 1 == tip_minute and 0 <= tip_second <= 10)
                     or (cur_hour + 1 == tip_hour and cur_minute == 59 and 0 <= tip_second <= 10)):
@@ -111,16 +150,20 @@ def Clicker():
     return 0
 
 
+#'''
 me = pyautogui.getActiveWindow()
 
 options = Options()
 options.add_argument('incognito')  # 시크릿 모드 실행
 options.add_argument("--start-maximized")  # 최대화
 # options.add_argument('--start-fullscreen') # F11
+options.add_experimental_option('excludeSwitches', ['enable-logging']) # https://stackoverflow.com/questions/69370457/google-chrome-always-crashes-after-opening-a-page
 driver = Chrome("chromedriver_win32\chromedriver", options=options)
 
-OpenZoom(
-    'https://ajou-ac-kr.zoom.us/rec/play/0NfxxPyRNHYklG9rQdKJIgGuHulsSUeMRChCOyJ7caOXxWiE-rUJOuC0NtgyoAWCDpZTSvmY4njhfBjN.UcPBJoQ-MYxgLRAE?continueMode=true&_x_zm_rtaid=XzEet155S32YMo6roMZXAw.1633655817998.cb8abb1a013236c89248563d3c7d96ca&_x_zm_rhtaid=114',
-    '3m!CnemD')
-# CrawlTimeline()
-Clicker()
+target_url = input("Zoom Video의 링크를 입력하세요: ")
+password = input("Zoom Video의 암호를 입력하세요: ")
+lecture_name = input("Zoom Video의 이름을 입력하세요(웬만하면 영어로 짧게): ")
+OpenZoom(target_url, password, lecture_name)
+CrawlTimeline(lecture_name)
+Clicker(lecture_name)
+#'''
